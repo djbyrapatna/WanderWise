96fbdabbe21bdfb19bdba06171234db5f832604a
Dhruva Byrapatna <dhruvajb@gmail.com>
2025-05-22 14:08:01 -0400

    WIP: add CI workflow and test packages

--- Modules/AuthCore/Sources/AuthCore/File 2.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File 2.swift
-//  AuthCore
-//
-//  Created by Dhruva Jothik Byrapatna on 5/22/25.
-//
-
-import Foundation
--- Modules/AuthCore/Tests/AuthCoreTests/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  AuthCore
-//
-//  Created by Dhruva Jothik Byrapatna on 5/22/25.
-//
-
-import Foundation
